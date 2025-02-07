4d1d895a1e2e85154f106f8f6d39818f0bfc42de
Park Juhyun <wngus6429@gmail.com>
2025-02-08 02:44:36 +0900

    유데미공부

--- foodies/app/page.js
+++ foodies/app/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import classes from "./page.module.css";
-import ImageSlideshow from "@/components/images/image-slideshow";
+import ImageSlideshow from "./components/images/image-slideshow.js";
 
 export default function Home() {
   return (
