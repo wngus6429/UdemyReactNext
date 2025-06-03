e331066ba6dc93fa4e44392ecc991cb0bf84bb24
Park Juhyun <wngus6429@gmail.com>
2025-06-04 02:20:47 +0900

    code/02/26

--- foodies/app/meals/share/page.js
+++ foodies/app/meals/share/page.js
@@ -1,5 +1,6 @@
+import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
-import ImagePicker from "../../components/meals/image-picker";
+import { shareMeal } from "@/lib/actions";
 
 export default function ShareMealPage() {
   return (
@@ -11,7 +12,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -34,7 +35,7 @@ export default function ShareMealPage() {
             <label htmlFor="instructions">Instructions</label>
             <textarea id="instructions" name="instructions" rows="10" required></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
